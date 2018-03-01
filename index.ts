6c97304e250ba206db850faf80a5d1951341143c
Huan LI <zixia@zixia.net>
2018-03-01 23:23:36 +0800

    add typescript scripts

---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+ * Here are your GraphQL Endpoints:
+
+  Simple API:        https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e
+  Relay API:         https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e
+  Subscriptions API: wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e
+ */
+
+export const SIMPLE_ENDPOINT = 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e'
+export const RELAY_ENDPOINT = 'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e'
+export const SUBSCRIPTIONS_ENDPOINT = 'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e'
