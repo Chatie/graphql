09ec898140126506a79009272af59c61bdae8765
Huan LI <zixia@zixia.net>
2018-03-07 22:46:41 +0800

    add smoke testing cases for query & mutation

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 /**
  * Here are your GraphQL Endpoints:
-
-  Simple API:        https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e
-  Relay API:         https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e
-  Subscriptions API: wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e
  */
 
-export const SIMPLE_ENDPOINT = 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e'
-export const RELAY_ENDPOINT = 'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e'
-export const SUBSCRIPTIONS_ENDPOINT = 'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e'
+export interface Endpoint {
+  simple:         string,
+  relay:          string,
+  subscriptions:  string,
+}
+
+export const ENDPOINT: Endpoint = {
+  simple: 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e',
+  relay:  'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e',
+  subscriptions:  'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e',
+}
+
+export const LOCAL_ENDPOINT: Endpoint = {
+  simple: 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e',
+  relay:  'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e',
+  subscriptions:  'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e',
+}
