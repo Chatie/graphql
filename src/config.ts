6c62eda89f9c6b9833db9bfac1fdc1e603f2d382
Huan LI <zixia@zixia.net>
2018-03-09 18:05:26 +0800

    add local server class

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+/**
+ * Here are your GraphQL Endpoints:
+ */
+
+export interface Endpoints {
+  simple:         string,
+  relay:          string,
+  subscriptions:  string,
+}
+
+export const ENDPOINTS: Endpoints = {
+  simple: 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e',
+  relay:  'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e',
+  subscriptions:  'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e',
+}
+
+/**
+ * graphcool info -t dev
+ */
+export const LOCAL_ENDPOINT: Endpoints = {
+  get simple() {
+    return 'https://api.graph.cool/simple/v1/cjdbw710k0zpr0129sv35o79e'
+  },
+  relay:  'https://api.graph.cool/relay/v1/cjdbw710k0zpr0129sv35o79e',
+  subscriptions:  'wss://subscriptions.ap-northeast-1.graph.cool/v1/cjdbw710k0zpr0129sv35o79e',
+}
