f00d3e399c2db2036e15767d99fbd9870c6847ad
Huan LI <zixia@zixia.net>
2018-03-14 00:00:06 +0800

    add docker clean/reset script (#3)

---
+++ scripts/local-reset.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+docker stop $(docker ps -f name=local_ -a -q)
+docker rm $(docker ps -f name=local_ -a -q)
+docker volume rm $(docker volume ls -q -f dangling=true -f name=local_)
