22e686c8e2ecf5bf498f20a6cf4b17a1930f5dfd
Huan LI <zixia@zixia.net>
2018-03-17 00:35:56 +0800

    back to home directory for configuration files

--- scripts/local-bootstrap.sh
+++ scripts/local-bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+# https://stackoverflow.com/a/1638397/1123955
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+DIR="$( cd $SCRIPTPATH/.. && pwd )"
+cd "$DIR"
+
+echo "Working dir: $DIR"
+
 echo 'graphcool local up...'
 graphcool local up
 echo 'done.'
